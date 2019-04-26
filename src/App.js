aca772dce4b2553a26589178cc60138d15b5dfc9
Vassil Genov <vg@Vassils-MacBook-Air-2.local>
2019-04-26 17:16:52 +0300

    how to get started with flexbox

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const GlobalStyle = createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
             "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
             sans-serif;
         -webkit-font-smoothing: antialiased;
--- src/components/Lesson02.js
+++ src/components/Lesson02.js
@@ -14,13 +14,30 @@ function Lesson02() {
 }
 
 const StyledContainer = styled.div`
+    border: 2px dashed red;
+    box-sizing: border-box;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    align-content: flex-end;
+    /* align-content: center; */
+    /* align-items: baseline; */
+    flex-wrap: wrap;
+
     > div {
         background: #373142;
         color: #fff;
         width: 100px;
         height: 100px;
         text-align: center;
-        line-height: 100px;
+        line-height: 150px;
+
+        &:nth-child(2) {
+            height: 250px;
+        }
     }
 `;
 
